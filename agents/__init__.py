c6a5d464acbe683b9934163e3e2e94540ef7a4a9
Rainforest Cheng <contact@rainforest.tools>
2025-02-23 12:35:05 +0800

    feat: implement caching mechanism for chat responses and update dependencies

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,12 +1,23 @@
+import tempfile
+from typing import Literal
 from autogen_core.models import UserMessage
-from autogen_ext.models.openai import OpenAIChatCompletionClient
 from autogen_ext.models.semantic_kernel import SKChatCompletionAdapter
+from pydantic import BaseModel
 from semantic_kernel import Kernel
 from semantic_kernel.connectors.ai.ollama import (
     OllamaChatCompletion,
     OllamaChatPromptExecutionSettings,
 )
 from semantic_kernel.memory.null_memory import NullMemory
+from autogen_ext.cache_store.diskcache import DiskCacheStore
+from autogen_ext.models.cache import CHAT_CACHE_VALUE_TYPE, ChatCompletionCache
+from diskcache import Cache
+
+
+class AgentResponse(BaseModel):
+    thoughts: str
+    response: Literal["happy", "sad", "neutral"]
+
 
 sk_client = OllamaChatCompletion(
     service_id="ollama",
@@ -21,25 +32,31 @@ model_client = SKChatCompletionAdapter(
 )
 
 messages = [
-    UserMessage(content="Write a very short story about a dragon.", source="user"),
+    UserMessage(content="Hello, how are you?", source="user"),
 ]
 
-stream = model_client.create_stream(messages=messages)
 
-
-async def process_stream():
-    async for response in stream:
-        if isinstance(response, str):
-            print(response, flush=True, end="")
-        else:
-            print("\n\n------------\n")
-            print("The complete response:", flush=True)
-            print(response.content, flush=True)
-            print("\n\n------------\n")
-            print("The token usage was:", flush=True)
-            print(response.usage, flush=True)
+async def main():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        cache_store = DiskCacheStore[CHAT_CACHE_VALUE_TYPE](Cache(temp_dir))
+        cache_client = ChatCompletionCache(model_client, cache_store)
+        stream = cache_client.create_stream(messages=messages)
+        # async for response in stream:
+        #     if isinstance(response, str):
+        #         print(response, flush=True, end="")
+        #     else:
+        #         print("\n\n------------\n")
+        #         print("The complete response:", flush=True)
+        #         print(response.content, flush=True)
+        #         print("\n\n------------\n")
+        #         print("The token usage was:", flush=True)
+        #         print(response.usage, flush=True)
+        response = await cache_client.create(messages=messages)
+        print(response)
+        response = await cache_client.create(messages=messages)
+        print(response)
 
 
 import asyncio
 
-asyncio.run(process_stream())
+asyncio.run(main())
